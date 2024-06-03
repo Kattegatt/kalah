93f469ac9aba92e4e721a3ddbe2998f14a8dc930
Kattegatt <kostenkodenys30@gmail.com>
2024-06-03 21:03:34 +0300

    socketio init

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 import express from "express";
+import cors from "cors";
 import mongoose from "mongoose";
+import http from "http";
+import { Server } from "socket.io";
 import userRouter from "./routers/UserRouter.js";
 import gameRouter from "./routers/GameRouter.js";
 
@@ -8,9 +11,35 @@ const PORT = process.env.PORT;
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/api", userRouter);
 app.use("/api", gameRouter);
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST", "PUT"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+
+  socket.on("joinGame", (gameId) => {
+    socket.join(gameId);
+    io.to(gameId).emit("newPlayer", { playerId: socket.id });
+  });
+
+  socket.on("move", (data) => {
+    io.to(data.gameId).emit("move", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
 
 async function startApp() {
   try {
