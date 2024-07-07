c91fd3f1cb7708317cc074c8900c3fb2eb6b0fd6
Kattegatt <kostenkodenys30@gmail.com>
2024-07-07 20:57:03 +0300

    Tutorial modal added

---
+++ client/src/components/TutorialModal.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="fixed inset-0 flex items-center justify-center z-10">
+    <div class="modal-content flex flex-col gap-4 p-6 bg-white rounded-lg shadow-lg">
+      <h2 class="text-xl font-bold text-center">Game Rules</h2>
+      <ul class="list-disc list-inside">
+        <li>
+          Moving counter-clockwise, the player deposits one of the stones in each pocket until the
+          stones run out.
+        </li>
+        <li>If the last piece you drop is in your own Mancala, you take another turn.</li>
+        <li>
+          If the last piece you drop is in an empty pocket on your side, you capture that piece and
+          any pieces in the pocket directly opposite.
+        </li>
+        <li>The game ends when all six pockets on one side of the Mancala board are empty.</li>
+        <li>
+          The player who still has pieces on his/her side of the board when the game ends captures
+          all of those pieces.
+        </li>
+        <li>The winner is the player with the most pieces.</li>
+      </ul>
+      <button @click="close" class="modal-button mt-4 py-2 px-4">Gotcha</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const emit = defineEmits(['close'])
+const close = () => {
+  emit('close')
+}
+</script>
+
+<style scoped>
+.modal-overlay {
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+.modal-content {
+  background-color: bisque;
+  max-width: 500px;
+  width: 100%;
+}
+</style>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -4,6 +4,7 @@
     <button @click="showLoginModal = true" class="btn">Login</button>
     <RegisterModal v-if="showRegisterModal" @close="showRegisterModal = false" />
     <LoginModal v-if="showLoginModal" @close="showLoginModal = false" />
+    <TutorialModal v-if="showTutorial" @close="showTutorial = false" />
   </div>
   <div class="flex items-center justify-center p-2 m-8">
     <h1 class="font-bold big text-2xl">kalah</h1>
@@ -11,18 +12,28 @@
   <div class="flex flex-col gap-3 items-center justify-center">
     <button class="menu-button">Single Player</button>
     <button class="menu-button">Online</button>
-    <button class="menu-button">Tutorial</button>
+    <button @click="showTutorial = true" class="menu-button">Tutorial</button>
+    <ul>
+      <li>asasdsadasd</li>
+      <li>asasdsadasd</li>
+    </ul>
   </div>
+  <RegisterModal v-if="showRegisterModal" @close="showRegisterModal = false" />
+  <LoginModal v-if="showLoginModal" @close="showLoginModal = false" />
+  <TutorialModal v-if="showTutorial" @close="showTutorial = false" />
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import RegisterModal from '../components/RegisterModal.vue'
 import LoginModal from '../components/LoginModal.vue'
+import TutorialModal from '../components/TutorialModal.vue'
+
 // import { useGameStore } from '../stores/game'
 
 const showRegisterModal = ref(false)
 const showLoginModal = ref(false)
+const showTutorial = ref(false)
 
 // const gameStore = useGameStore()
 </script>
