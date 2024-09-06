85ca73186dafd1a69934a4fcb3ff8b42659f550b
Kattegatt <kostenkodenys30@gmail.com>
2024-09-06 12:12:23 +0300

    Register modal changed to naive-ui Validation setted up

--- client/src/components/RegisterModal.vue
+++ client/src/components/RegisterModal.vue
@@ -1,53 +1,110 @@
 <template>
-  <div class="fixed inset-0 flex items-center justify-center yorha-background z-10 background">
-    <form @submit.prevent="register">
-      <fieldset class="p-5">
-        <legend><h1>Sign Up</h1></legend>
-        <input v-model="username" type="text" placeholder="Username" required class="input" />
-        <input v-model="email" type="email" placeholder="Email" required class="input" />
-        <input v-model="password" type="password" placeholder="Password" required class="input" />
-        <input
-          v-model="repeatPassword"
+  <n-card
+    style="width: 600px"
+    title="Sign Up"
+    :bordered="false"
+    size="huge"
+    role="dialog"
+    aria-modal="true"
+  >
+    <n-form ref="formRef" :model="formValue" :rules="rules">
+      <n-form-item label="Username" path="username">
+        <n-input v-model:value="formValue.username" type="text" placeholder="Username" />
+      </n-form-item>
+
+      <n-form-item label="Email" path="email">
+        <n-input v-model:value="formValue.email" placeholder="Email" />
+      </n-form-item>
+
+      <n-form-item label="Password" path="password">
+        <n-input v-model:value="formValue.password" type="password" placeholder="Password" />
+      </n-form-item>
+
+      <n-form-item label="Repeat Password" path="repeatPassword">
+        <n-input
+          v-model:value="formValue.repeatPassword"
           type="password"
           placeholder="Repeat Password"
-          required
-          class="input"
         />
-        <button type="submit" class="btn">Register</button>
-        <button type="button" @click="$emit('close')" class="btn ml-2">Cancel</button>
-      </fieldset>
-    </form>
-  </div>
+      </n-form-item>
+
+      <n-button @click="register" :disabled="isButtonDisabled" class="btn">Register</n-button>
+      <n-button @click="$emit('close')" class="btn ml-2">Cancel</n-button>
+    </n-form>
+  </n-card>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
 import { baseUrl, endpoints } from '../utils/api/endpoints'
 
+//components
+import { NButton, NInput, NFormItem, NCard, NForm } from 'naive-ui'
+
 const emit = defineEmits(['close', 'signedIn'])
 
-const missClickEventListener = (element) => {
-  element.addEventListener('click', (event) => {
-    if (event.target === element) {
-      element.removeEventListener('click', null)
-      emit('close')
-    }
-  })
-}
-onMounted(() => {
-  const background = document.getElementsByClassName('background')[0]
-  setTimeout(() => missClickEventListener(background), 100)
+const formRef = ref(null)
+const formValue = ref({
+  username: '',
+  email: '',
+  password: '',
+  repeatPassword: ''
 })
 
-const username = ref('')
-const email = ref('')
-const password = ref('')
-const repeatPassword = ref('')
+// naive validation
+const rules = {
+  username: {
+    required: true,
+    message: 'Please input your username',
+    trigger: ['input']
+  },
+  email: [
+    {
+      required: true,
+      message: 'Please input your email',
+      trigger: ['input']
+    },
+    {
+      validator: (rule, value) => {
+        if (!value) return true
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+        return emailRegex.test(value)
+      },
+      message: 'Please input a valid email',
+      trigger: ['blur', 'input']
+    }
+  ],
+
+  password: {
+    required: true,
+    message: 'Please input your number',
+    trigger: ['input']
+  },
+  repeatPassword: [
+    {
+      required: true,
+      message: 'Please repeat your password',
+      trigger: ['input']
+    },
+    {
+      validator: validatePasswordSame,
+      message: 'Password is not same as re-entered password!',
+      trigger: ['blur', 'password-input']
+    }
+  ]
+}
+
+function validatePasswordSame(rule, value) {
+  return value === formValue.value.password
+}
 
 const register = async () => {
+  console.log(formValue.value.password)
+  formRef.value?.validate()
+
   // validation of required inputs nedded
-  if (password.value !== repeatPassword.value) {
-    alert('Passwords do not match')
+  if (formValue.value.password !== formValue.value.repeatPassword) {
+    console.log('Not same passwords')
     return
   }
 
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,21 +1,24 @@
 <template>
   <div v-if="!isSignedIn" class="flex justify-end gap-2">
-    <button @click="showRegisterModal = true" class="btn">Sign Up</button>
-    <button @click="showLoginModal = true" class="btn">Sign In</button>
+    <n-button @click="showRegisterModal = true" class="btn">Sign Up</n-button>
+    <n-button @click="showLoginModal = true" class="btn">Sign In</n-button>
+    <!-- <button @click="showRegisterModal = true" class="btn">Sign Up</button> -->
+    <!-- <button @click="showLoginModal = true" class="btn">Sign In</button> -->
   </div>
   <div class="flex items-center justify-center p-2 m-8">
-    <h1 class="font-bold big text-2xl">kalah</h1>
+    <h1 class="font-bold big text-2xl">Kalah</h1>
   </div>
   <div class="flex flex-col gap-3 items-center justify-center">
-    <button class="menu-button" @click="SinglePlayerRoute">Single Player</button>
-    <button v-if="isSignedIn" class="menu-button" @click="lobbyRoute">Online</button>
-    <button @click="showTutorial = true" class="menu-button">Tutorial</button>
+    <n-button class="menu-button" type="tertiary" @click="SinglePlayerRoute"
+      >Single Player</n-button
+    >
+    <n-button v-if="isSignedIn" class="menu-button" @click="lobbyRoute">Online</n-button>
+    <n-button @click="showTutorial = true" class="menu-button">Tutorial</n-button>
   </div>
-  <RegisterModal
-    v-if="showRegisterModal"
-    @signedIn="setSignedIn"
-    @close="showRegisterModal = false"
-  />
+  <n-modal v-model:show="showRegisterModal">
+    <RegisterModal @signedIn="setSignedIn" @close="showRegisterModal = false" />
+  </n-modal>
+
   <LoginModal v-if="showLoginModal" @signedIn="setSignedIn" @close="showLoginModal = false" />
   <TutorialModal v-if="showTutorial" @close="showTutorial = false" />
 </template>
@@ -28,6 +31,9 @@ import TutorialModal from '../components/TutorialModal.vue'
 import { usePlayerStore } from '../stores/player'
 import router from '../router/index.js'
 import SinglePlayer from './SinglePlayer.vue'
+
+//components
+import { NButton, NModal } from 'naive-ui'
 
 const lobbyRoute = () => {
   router.push({ path: '/lobby' })
@@ -56,5 +62,6 @@ const showTutorial = ref(false)
 .menu-button {
   width: 12rem;
   height: 3rem;
+  @apply py-2 px-4;
 }
 </style>
