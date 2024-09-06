4414d7f1b6094733116e257c232075c9d24d3efb
Kattegatt <kostenkodenys30@gmail.com>
2024-09-06 14:02:59 +0300

    Tutorial modal changed to naive-ui

--- client/src/components/TutorialModal.vue
+++ client/src/components/TutorialModal.vue
@@ -1,34 +1,48 @@
 <template>
-  <div class="fixed inset-0 flex items-center justify-center z-10">
-    <div class="modal-content flex flex-col gap-4 p-6 bg-white rounded-lg shadow-lg">
-      <h2 class="text-xl font-bold text-center mb-4">Game Rules</h2>
-      <ul class="list-disc list-inside">
-        <li>
+  <n-card style="width: 600px" title="Game Rules">
+    <n-list hoverable>
+      <n-thing>
+        <n-list-item>
           Moving counter-clockwise, the player deposits one of the stones in each pocket until the
           stones run out.
-        </li>
-        <li>If the last piece you drop is in your own Mancala, you take another turn.</li>
-        <li>
+        </n-list-item>
+      </n-thing>
+      <n-thing>
+        <n-list-item>
+          If the last piece you drop is in your own Mancala, you take another turn.
+        </n-list-item>
+      </n-thing>
+      <n-thing>
+        <n-list-item>
           If the last piece you drop is in an empty pocket on your side, you capture that piece and
           any pieces in the pocket directly opposite.
-        </li>
-        <li>The game ends when all six pockets on one side of the Mancala board are empty.</li>
-        <li>
+        </n-list-item>
+      </n-thing>
+      <n-thing>
+        <n-list-item>
+          The game ends when all six pockets on one side of the Mancala board are empty.
+        </n-list-item>
+      </n-thing>
+      <n-thing>
+        <n-list-item>
           The player who still has pieces on his/her side of the board when the game ends captures
           all of those pieces.
-        </li>
-        <li>The winner is the player with the most pieces.</li>
-      </ul>
-      <button @click="close" class="modal-button mt-4 py-2 px-4">Gotcha</button>
-    </div>
-  </div>
+        </n-list-item>
+      </n-thing>
+      <n-thing>
+        <n-list-item>The winner is the player with the most pieces.</n-list-item>
+      </n-thing>
+    </n-list>
+
+    <n-button class="mt-2" type="primary" @click="emit('close')">Gotcha</n-button>
+  </n-card>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import { NCard, NThing, NList, NListItem, NButton } from 'naive-ui'
+
 const emit = defineEmits(['close'])
-const close = () => {
-  emit('close')
-}
 </script>
 
 <style scoped>
